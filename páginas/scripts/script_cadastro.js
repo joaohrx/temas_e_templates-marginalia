b9e4459ed3763ab8ea61268eedbe19bd595f6c24
Edson2025-dev <edsonfelix188@gmail.com>
2025-07-31 22:46:11 -0300

    opa, hoje 31/07/2025 coloquei validação da tela de login e cadastro com Java script

--- páginas/scripts/script_cadastro.js
+++ páginas/scripts/script_cadastro.js
@@ -1,6 +1,4 @@
-/*área de cadastro*/
-
-        const themeToggleBtn = document.getElementById('theme-toggle');
+const themeToggleBtn = document.getElementById('theme-toggle');
         const body = document.body;
 
       
@@ -35,5 +33,30 @@
                 body.classList.add('light-theme'); 
                 localStorage.setItem('theme', 'light-theme');
                 updateButtonText('light-theme');
+            }
+        });
+
+       
+
+
+            document.getElementById('loginForm').addEventListener('submit', function(event) {
+            event.preventDefault(); 
+             
+
+            const nome = document.getElementById('nome').value.trim();
+            const email = document.getElementById('email').value.trim();
+            const senha = document.getElementById('senha').value.trim();
+            const telefone = document.getElementById('telefone').value.trim();
+            const endereco = document.getElementById('endereco').value.trim();
+            const data_de_nascimento = document.getElementById('data_de_nascimento').value.trim();
+
+            const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (nome === '' || email === '' || senha === '' || telefone === '' || endereco === '' || data_de_nascimento === '') {
+                alert('Por favor, preencha todos os campos.');
+            } else if (!emailPattern.test(email)) {
+                alert('Por favor, insira um email válido.');
+            } else {
+                alert('Formulário enviado com sucesso!');
+                document.getElementById('loginForm').reset();
             }
         });
