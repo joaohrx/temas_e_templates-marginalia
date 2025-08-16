920bd2072d3e3ff18d1ff25edc526bd5ee7ffccb
joaohrx <victor.rocha1@escolar.ifrn.edu.br>
2025-08-16 20:19:41 -0300

    ajuste login

--- páginas/scripts/script_tela_de_login.js
+++ páginas/scripts/script_tela_de_login.js
@@ -1,19 +1,18 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
 
+    const nome = document.getElementById('nome').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const senha = document.getElementById('senha').value.trim();
 
-            document.getElementById('loginForm').addEventListener('submit', function(event) {
-                event.preventDefault(); 
-
-                const nome = document.getElementById('nome').value.trim();
-                const email = document.getElementById('email').value.trim();
-                const senha = document.getElementById('senha').value.trim();
-
-                const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-                if (nome === '' || email === '' || senha === '') {
-                    alert('Por favor, preencha todos os campos.');
-                } else if (!emailPattern.test(email)) {
-                    alert('Por favor, insira um email válido.');
-                } else {
-                    alert('Formulário enviado com sucesso!');
-                    document.getElementById('loginForm').reset();
-                }
-            });
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (nome === '' || email === '' || senha === '') {
+        alert('Por favor, preencha todos os campos.');
+    } else if (!emailPattern.test(email)) {
+        alert('Por favor, insira um email válido.');
+    } else {
+        alert('Formulário enviado com sucesso!');
+        this.reset();
+        window.location.href = "conta_usuario.html";
+}
+});
